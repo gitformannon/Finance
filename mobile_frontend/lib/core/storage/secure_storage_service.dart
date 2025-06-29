d270fe66973f64d96783c352c38a43b0ea85098d
gitformannon <vladimirovmannon@gmail.com>
2025-06-29 21:37:22 +0500

    feat: secure local token storage

---
+++ mobile_frontend/lib/core/storage/secure_storage_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:hive/hive.dart';
+
+import '../constants/local_storage_keys.dart';
+
+class SecureStorageService {
+  static const FlutterSecureStorage _storage = FlutterSecureStorage();
+
+  static Future<Uint8List> getEncryptionKey() async {
+    String? encodedKey =
+        await _storage.read(key: LocalStorageKeys.encryptionKey);
+    if (encodedKey == null) {
+      final Uint8List key = Hive.generateSecureKey();
+      encodedKey = base64UrlEncode(key);
+      await _storage.write(key: LocalStorageKeys.encryptionKey, value: encodedKey);
+      return key;
+    }
+    return base64Url.decode(encodedKey);
+  }
+}
