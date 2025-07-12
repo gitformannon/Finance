522fa78ea8d890cf7c66653a45d8f37d1538fdf7
gitformannon <vladimirovmannon@gmail.com>
2025-07-12 18:28:31 +0500

    Refine register fields and cleanup

--- mobile_frontend/lib/features/auth/presentation/pages/forgot_password.dart
+++ mobile_frontend/lib/features/auth/presentation/pages/forgot_password.dart
@@ -8,10 +8,8 @@ import '../../../../core/constants/app_sizes.dart';
 import '../../../../core/constants/locale_keys.dart';
 import '../../../shared/presentation/cubits/navigate/navigate_cubit.dart';
 import '../../../shared/presentation/widgets/textfields/w_masked_textfield.dart';
-import '../../../shared/presentation/widgets/textfields/w_search_textfield.dart';
-import '../../../shared/presentation/widgets/textfields/app_text_field.dart';
 import '../../../shared/presentation/widgets/app_buttons/w_button.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import '../../../shared/presentation/widgets/app_buttons/w_text_button.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({super.key});
@@ -73,7 +71,7 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
                           text: 'Send',
                         ),
                         SizedBox(height: AppSizes.spaceM16.h),
-                        WButton(
+                        WTextButton(
                           onTap: () =>
                               context.read<NavigateCubit>().goToLoginPage(),
                           text: 'Back to login',
--- mobile_frontend/lib/features/auth/presentation/pages/register.dart
+++ mobile_frontend/lib/features/auth/presentation/pages/register.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../../core/constants/app_colors.dart';
 import '../../../../core/constants/app_sizes.dart';
 import '../../../../core/constants/locale_keys.dart';
 import '../../../shared/presentation/cubits/navigate/navigate_cubit.dart';
 import '../../../shared/presentation/widgets/app_buttons/w_button.dart';
+import '../../../shared/presentation/widgets/app_buttons/w_text_button.dart';
 import '../../../shared/presentation/widgets/textfields/w_masked_textfield.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -19,24 +19,24 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  final _usernameController = TextEditingController();
   final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
+  final _codeController = TextEditingController();
+  final _newPasswordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
 
   String? _errorMessage;
 
   @override
   void dispose() {
-    _usernameController.dispose();
     _emailController.dispose();
-    _passwordController.dispose();
+    _codeController.dispose();
+    _newPasswordController.dispose();
     _confirmPasswordController.dispose();
     super.dispose();
   }
 
   void _register() {
-    if (_passwordController.text != _confirmPasswordController.text) {
+    if (_newPasswordController.text != _confirmPasswordController.text) {
       setState(() {
         _errorMessage = 'Passwords do not match';
       });
@@ -71,18 +71,18 @@ class _RegisterPageState extends State<RegisterPage> {
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
                       WMaskedTextField(
-                        controller: _usernameController,
-                        hintText: LocaleKeys.userName.tr(),
-                      ),
-                      SizedBox(height: AppSizes.spaceXXS5.h),
-                      WMaskedTextField(
                         controller: _emailController,
                         hintText: 'Email',
                       ),
                       SizedBox(height: AppSizes.spaceXXS5.h),
                       WMaskedTextField(
-                        controller: _passwordController,
-                        hintText: LocaleKeys.password.tr(),
+                        controller: _codeController,
+                        hintText: 'Code',
+                      ),
+                      SizedBox(height: AppSizes.spaceXXS5.h),
+                      WMaskedTextField(
+                        controller: _newPasswordController,
+                        hintText: 'New password',
                         isPassword: true,
                       ),
                       SizedBox(height: AppSizes.spaceXXS5.h),
@@ -106,15 +106,10 @@ class _RegisterPageState extends State<RegisterPage> {
                         text: 'Register',
                       ),
                       SizedBox(height: AppSizes.spaceL20.h),
-                      WButton(
+                      WTextButton(
                         onTap:
                             () => context.read<NavigateCubit>().goToLoginPage(),
                         text: 'Back to login',
-                        hasPreviousIcon: true,
-                        prevIcon: SvgPicture.asset(
-                          'assets/svg/ic_arrow_left.svg',
-                          fit: BoxFit.scaleDown,
-                        ),
                       ),
                     ],
                   ),
