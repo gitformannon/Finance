3ba7033f726bca71ec723e6a9e3c80ac6da0d3a3
gitformannon <vladimirovmannon@gmail.com>
2025-07-13 22:09:39 +0500

    Finance FE AppBar height

--- mobile_frontend/lib/core/navigation/app_pages.dart
+++ mobile_frontend/lib/core/navigation/app_pages.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:Finance/features/main/presentation/pages/main_page.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
