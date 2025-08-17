09c019a20ca97b31c16b02ec0e45f7e0df8a995e
gitformannon <vladimirovmannon@gmail.com>
2025-08-17 23:43:23 +0500

    FE IncomeCategoryItem add_tr_modal

--- mobile_frontend/lib/features/budget/presentation/pages/add_transaction_modal.dart
+++ mobile_frontend/lib/features/budget/presentation/pages/add_transaction_modal.dart
@@ -2,6 +2,7 @@ import 'package:Finance/core/constants/app_colors.dart';
 import 'package:Finance/core/constants/app_sizes.dart';
 import 'package:Finance/core/themes/app_text_styles.dart';
 import 'package:Finance/features/budget/presentation/widgets/add_category_item.dart';
+import 'package:Finance/features/budget/presentation/widgets/bottom_datepicker_modal.dart';
 import 'package:Finance/features/budget/presentation/widgets/category_item.dart';
 import 'package:Finance/features/budget/presentation/widgets/transaction_type_button.dart';
 import 'package:Finance/features/budget/presentation/widgets/transfer_account_item.dart';
@@ -246,8 +247,11 @@ class _AddTransactionModalState extends State<AddTransactionModal> {
                             Row(
                               children: [
                                 GestureDetector(
-                                  onTap:
-                                      () => _showCalendarPicker(context, cubit),
+                                  onTap: () => BottomDatepickerModal.show(
+                                  context,
+                                  initialDate: cubit.state.date,
+                                  onSelect: cubit.setDate,
+                                  ),
                                   child: Row(
                                     children: [
                                       Container(
--- mobile_frontend/lib/features/budget/presentation/widgets/bottom_datepicker_modal.dart
+++ mobile_frontend/lib/features/budget/presentation/widgets/bottom_datepicker_modal.dart
@@ -1,10 +1,135 @@
+import 'package:Finance/core/constants/app_colors.dart';
+import 'package:Finance/core/constants/app_sizes.dart';
+import 'package:Finance/features/shared/presentation/widgets/app_buttons/w_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
 
-class BottomDatepickerModal extends StatelessWidget {
-  const BottomDatepickerModal({super.key});
+class BottomDatepickerModal extends StatefulWidget {
+  final DateTime initialDate;
+  final ValueChanged<DateTime> onSelect;
+
+  const BottomDatepickerModal({
+    super.key,
+    required this.initialDate,
+    required this.onSelect,
+  });
+
+  static Future<void> show(
+      BuildContext context, {
+        required DateTime initialDate,
+        required ValueChanged<DateTime> onSelect,
+      }) {
+    return showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      useSafeArea: true,
+      builder: (_) => BottomDatepickerModal(
+        initialDate: initialDate,
+        onSelect: onSelect,
+      ),
+    );
+  }
+
+  @override
+  State<BottomDatepickerModal> createState() => _BottomDatepickerModalState();
+}
+
+class _BottomDatepickerModalState extends State<BottomDatepickerModal> {
+  late DateTime _tempDate;
+
+  @override
+  void initState() {
+    super.initState();
+    _tempDate = widget.initialDate;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(AppSizes.borderSM16),
+        topRight: Radius.circular(AppSizes.borderSM16),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            height: 200.h,
+            color: AppColors.background,
+            child: CupertinoDatePicker(
+              initialDateTime: _tempDate,
+              mode: CupertinoDatePickerMode.date,
+              backgroundColor: AppColors.background,
+              onDateTimeChanged: (d) => setState(() => _tempDate = d),
+            ),
+          ),
+          Container(
+            color: AppColors.background,
+            child: SafeArea(
+              top: false,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: AppSizes.paddingM.w,
+                  right: AppSizes.paddingM.w,
+                ),
+                child: WButton(
+                  onTap: () {
+                    widget.onSelect(_tempDate);
+                    Navigator.of(context).pop();
+                  },
+                  text: 'Select',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomDatepickerField extends StatelessWidget {
+  BottomDatepickerField({
+    super.key,
+    required this.date,
+    required this.onSelect
+  })
+
+  final DateTime date;
+  final ValueChanged<DateTime> onSelect;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => BottomDatepickerModal.show(
+        context,
+        initialDate: date,
+        onSelect: onSelect,
+      ),
+      child: Row(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.def.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(AppSizes.borderSmall),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(AppSizes.paddingS),
+              child: Icon(
+                Icons.calendar_today,
+                size: 24,
+                color: AppColors.textSecondary,
+              ),
+            ),
+          ),
+          SizedBox(width: AppSizes.spaceXS8.w),
+          Text(
+            DateFormat('dd MMMM yyyy').format(date),
+          ),
+        ],
+      ),
+    );
   }
 }
